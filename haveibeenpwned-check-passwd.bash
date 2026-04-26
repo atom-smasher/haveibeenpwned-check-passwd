d15345e940811cd0dd137e9faff5419d6a084ebb
atom-smasher <xatomx@gmail.com>
2026-04-26 20:06:36 +1200

    haveibeenpwned-check-passwd.bash
    v1.04 - 26 apr 2026

--- haveibeenpwned-check-passwd.bash
+++ haveibeenpwned-check-passwd.bash
@@ -3,7 +3,7 @@
 #######################################
 ## atom smasher's haveibeenpwned-check-passwd.bash
 ## https://github.com/atom-smasher/haveibeenpwned-check-passwd
-## v1.03       26 apr 2026
+## v1.04 - 26 apr 2026
 ## Distributed under the GNU General Public License
 ## http://www.gnu.org/copyleft/gpl.html
 
@@ -12,6 +12,18 @@
 ## https://haveibeenpwned.com/api/v3
 
 pw="${*}"
+
+## test if no agument is given, and request a password
+[[ "${pw}" ]] || {
+    ## if the script is killed, restore output to terminal
+    trap "stty echo" EXIT
+    ## turn off echo
+    stty -echo
+    ## read secret
+    read -p 'Password: ' pw
+    ## turn on echo
+    stty echo
+}
 
 sha=$(printf '%s' "${pw}" | sha1sum | awk '{print toupper($1)}')
 prefix=${sha:0:5}
@@ -29,7 +41,7 @@ trap - ERR
 
 ## if there's a match, print that info and `exit 0`
 line=$(echo "${match}" | grep  "^${suffix}:")
-[[ -n "${line}" ]] && {
+[[ "${line}" ]] && {
     echo "${prefix}${line}" | awk -F: '{print "SHA1:    "$1"\nMatches: "$2}'
     exit 0
 }
